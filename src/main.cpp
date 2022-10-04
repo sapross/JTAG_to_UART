2144247009ab251ba5e1243350d41b7dd3f9851d
Stephan Proß <s.pross@stud.uni-heidelberg.de>
2022-10-04 17:36:37 +0200

    Added main starting server.

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,19 @@
 #include <string>
 #include <termios.h>
 
+#include "Adapter.hpp"
+#include "JTAGDevice.hpp"
 #include "TCPServer.hpp"
 #include "UARTdevice.hpp"
 
 int main(int argc, char* argv[])
 {
-    // std::string serial_device = "/dev/pts/7";
-    // UARTDevice  dev(serial_device, B115200);
-    // dev.send("Hello World!");
-    // dev.receive(3);
+    std::string serial_device = "/dev/pts/7";
+    UARTDevice  uart(serial_device, B115200);
+    Adapter     adapter(uart);
+    JTAGDevice  jtag(adapter);
+    TCPServer   server(jtag);
 
-    TCPServer server;
     server.join();
 
     return 0;
