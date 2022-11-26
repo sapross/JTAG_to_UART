f4807fba2398037023d093b4ba638ef1bbfbc458
Stephan Proß <s.pross@stud.uni-heidelberg.de>
2022-11-26 16:12:33 +0100

    Added Lyra for CLI processing.

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,34 @@
 #include <string>
 #include <termios.h>
 
+#include <lyra/lyra.hpp>
+
 #include "Adapter.hpp"
 #include "JTAGDevice.hpp"
 #include "TCPServer.hpp"
 #include "UARTdevice.hpp"
 
-int main(int argc, char* argv[])
+int main(int argc, char** argv)
 {
-    std::string serial_device = "/dev/pts/7";
-    if (argc > 1)
+    std::string tty      = "/dev/pts/7";
+    int         baudrate = 115200;
+    auto        cli      = lyra::cli() | lyra::opt(tty, "tty")["-t"]["--tty"]("Path to the serial device to use.") |
+               lyra::opt(baudrate, "baudrate")["-b"]["--baudrate"]("Baudrate of target device.");
+
+    auto result = cli.parse({argc, argv});
+    if (!result)
     {
-        serial_device = argv[1];
+        std::cerr << "Error in command line: " << result.message() << std::endl;
+        return 1;
     }
-    UARTDevice uart(serial_device, B115200); // B3000000);
+    unsigned int bd = get_baudrate(baudrate);
+    if (bd == 0)
+    {
+        std::cerr << "Error: Invalid Baudrate " << baudrate << std::endl;
+        return 1;
+    }
+
+    UARTDevice uart(tty, bd); // B3000000);
     Adapter    adapter(uart);
     JTAGDevice jtag(adapter);
     TCPServer  server(jtag);
